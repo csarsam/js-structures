3497328cd10155ed50dffe652f96f8128f37d38d
Claire Sarsam <csarsam@gmail.com>
2014-03-24 13:42:06 -0700

    Initial commit

---
+++ binary_search_tree/BinarySearchTree.js
@@ -0,0 +1,60 @@
+var BinarySearchTree = function (key) {
+  'use strict';
+  this._root = undefined;
+  this._key = key;
+};
+
+BinarySearchTree.prototype = {
+  /* 
+   * Public methods
+   */
+
+  insert: function (node) {
+
+  },
+
+  delete: function (key) {
+
+  },
+
+  search: function (key) {
+
+  },
+
+  inorder_walk: function() {
+
+  },
+
+  preorder_walk: function() {
+
+  },
+
+  postorder_walk: function() {
+
+  },
+
+  minimum: function(tree) {
+
+  },
+
+  maximum: function(tree) {
+
+  },
+
+ /*
+  * Private methods
+  */
+
+  _transplant: function (tree, u, v) {
+
+  },
+
+  _predecessor: function (x) {
+
+  },
+
+  _successor: function (x) {
+
+  }
+
+};
